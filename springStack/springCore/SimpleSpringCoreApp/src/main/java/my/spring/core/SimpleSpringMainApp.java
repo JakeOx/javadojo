df16b23db75842be50ac4f8893dbb159154a2b02
Jacob Verghese <j.verghese@oberthur.com>
2019-01-11 13:06:56 -0500

    Added SpringStack projects

---
+++ springStack/springCore/SimpleSpringCoreApp/src/main/java/my/spring/core/SimpleSpringMainApp.java
@@ -0,0 +1,17 @@
+package my.spring.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SimpleSpringMainApp {
+	
+		   public static void main(String[] args) {
+		      ApplicationContext ctx = 
+		         new AnnotationConfigApplicationContext(TextEditorConfig.class);
+
+		      TextEditor te = ctx.getBean(TextEditor.class);
+		      te.spellCheck();
+		   }
+	
+
+}
