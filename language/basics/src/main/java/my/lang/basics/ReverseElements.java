2cbbaf3e923080fcaf31b7cdfb58bc5d54fb3961
Jacob Verghese <j.verghese@oberthur.com>
2019-06-23 21:54:14 -0400

    Added Reverse sorting techniques

---
+++ language/basics/src/main/java/my/lang/basics/ReverseElements.java
@@ -0,0 +1,52 @@
+package my.lang.basics;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReverseElements {
+	
+	public static void main(String[] args) {
+		
+		List<String> names = Arrays.asList("One", "Two", "Three", "Four");
+		reverse_stream_print(names);
+		
+	}
+	
+	public static void  reverse_stream_print(List<String> elements) {
+
+	    Stream.of(elements)
+	            .collect(Collectors.toCollection(LinkedList::new))
+	            .descendingIterator().forEachRemaining(System.out::println);
+	}
+	
+	public void sorting_reverse(List<String> elements) {
+
+	    Stream.of(elements)
+	            .sorted(Collections.reverseOrder())
+	            .forEach(System.out::println);
+
+	}
+	
+	
+	public void sorting_basic(List<String> elements){
+		
+		Collections.reverse(elements);
+		elements.forEach(System.out::println);
+	}
+	
+	public void sorting_veryBasic(List<String> elements){
+		
+		Object[] arrayElems = elements.toArray();
+		
+		for (int i=arrayElems.length-1; i>=0; i--){
+			System.out.println(arrayElems[i]);
+		}
+	}
+	
+	
+
+}
