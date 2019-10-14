48fe50353f7f97435628fa2b7cbdac2514ed96ea
Jacob Verghese <j.verghese@oberthur.com>
2019-10-13 22:07:53 -0400

    Updated to check Assertion when assertions are enabled in visualStudioCode workspace settings launch.json

--- collections/my_enums/src/main/java/com/jak/collections/MyEnums.java
+++ collections/my_enums/src/main/java/com/jak/collections/MyEnums.java
@@ -3,14 +3,36 @@ package com.jak.collections;
 import java.util.EnumMap;
 
 public class MyEnums{
+
+    EnumMap<DaysOfWeek, String> schedule;
     
     public static void main(String[] args){
-        System.out.println("Hello");
+        
+        //Build and print the Enums
+        MyEnums doEnums = new MyEnums();
+        doEnums.buildMap();
+        doEnums.printMap();
     }
 
    public enum DaysOfWeek{ MONDAY, TUESDAY, WEDNESDAY, THRUSDAY, FRIDAY, SATURDAY, SUNDAY}
 
    public void buildMap(){
-       EnumMap<DaysOfWeek, String> schedule = new EnumMap<>(DaysOfWeek.class);
+       schedule = new EnumMap<>(DaysOfWeek.class);
+       
+       schedule.put(DaysOfWeek.MONDAY, "Soccer");
+       schedule.put(DaysOfWeek.TUESDAY, "Basketball");
+       schedule.put(DaysOfWeek.WEDNESDAY, "Karate");
+       schedule.put(DaysOfWeek.THRUSDAY, "Football");
+       schedule.put(DaysOfWeek.FRIDAY, "Baseball");
+       schedule.put(DaysOfWeek.SATURDAY, "Lacrose");
+       schedule.put(DaysOfWeek.SUNDAY, "Church");
+    
+
+       assert schedule.get(DaysOfWeek.TUESDAY).equalsIgnoreCase("Basketball");
+       assert (schedule.size() == 7);
+   }
+
+   public void printMap(){
+    schedule.values().stream().forEach(System.out::println);
    }
 }
